043f19dc66e59c7d1dad63ec9b7b39b1e3da6617
Florian Dunz <dunz.florian@web.de>
2018-09-02 17:52:55 +0200

    added level 10, added spinning spikes
    fixed stuck in level 8
    adjustment to background

---
+++ GMTKJam2018/Assets/Scripts/Objects/SpinningSpike.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningSpike : MonoBehaviour {
+    [SerializeField]
+    private float speed = 120f;
+
+    void Update() {
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+    }
+}
