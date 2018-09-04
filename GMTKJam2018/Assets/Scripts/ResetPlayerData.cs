eec0eab0cb7bcdfe78795221723fdd90e4916579
Florian Dunz <dunz.florian@web.de>
2018-09-04 19:42:53 +0200

    fixed deaths not resetting when pressing esc and playing again, fixed unused entities being visible in level 9

---
+++ GMTKJam2018/Assets/Scripts/ResetPlayerData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayerData : MonoBehaviour {
+	void Start () {
+        PlayerData.Deaths = 0;
+	}
+}
